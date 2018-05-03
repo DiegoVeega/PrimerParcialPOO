706a94682bda439f1c77992677649a0b6f2b1bb6
Diego Vega <00148816@uca.edu.sv>
2018-05-02 22:19:10 -0600

    ArrayList de las habitaciones, base de la clase

---
+++ src/Sistema/RegistroHabitacion.java
@@ -0,0 +1,18 @@
+package Sistema;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author diego
+ */
+public class RegistroHabitacion extends Habitacion{
+    //Atributos.
+    private ArrayList<Habitacion> habitaciones;
+    
+    public RegistroHabitacion(){
+        habitaciones=new ArrayList<>();
+    }
+    //Metodos.
+    
+}
