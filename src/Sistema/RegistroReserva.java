4c67032cfd7adfd8e62a5146e74807bb8b36a051
Diego Vega <00148816@uca.edu.sv>
2018-05-03 00:32:48 -0600

    Se realizo Refactor para colocar clase agregada por otro miembro en el paquete correcto

---
+++ src/Sistema/RegistroReserva.java
@@ -0,0 +1,16 @@
+package Sistema;
+
+
+import java.util.ArrayList;
+        
+/**
+ *
+ * @author Julio Flores <00081817@uca.edu.sv>
+ */
+public class RegistroReserva {
+    private final ArrayList<Reserva> reservas;
+    
+    public RegistroReserva(){
+        reservas = new ArrayList<>();
+    }
+}
