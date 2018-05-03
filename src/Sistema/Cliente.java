5c1643acaedfa1ccf27985b40bf40490ac45a4e0
cceren <cceren@harding.edu>
2018-05-02 22:30:56 -0600

    annadi la clase cliente

---
+++ src/Sistema/Cliente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sistema;
+
+/**
+ *
+ * @author Kriz
+ */
+public class Cliente {
+    private String id;
+    private String Nombre;
+    private String Apellido;
+    private String Direccion;
+    public Habitacion habitacion;
+    private String id;
+}
