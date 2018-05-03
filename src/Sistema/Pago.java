599791fd11290bc7999d77a612a0ef2039cb0a6b
cceren <cceren@harding.edu>
2018-05-03 00:28:17 -0600

    indicaciones para completar clase pago

--- src/Sistema/Pago.java
+++ src/Sistema/Pago.java
@@ -9,6 +9,7 @@ public class Pago {
     private double MontoTotal;
     private Fecha FechaPago;
     public Cliente Cliente;
+    public Habitacion CostoHabitacion;
     private String Codigo;
     /*----------------------------------ATRIBUTOS NECESARIOS PARA CALCULAR EL MONTO TOTAL------------------------
     public Piso CostoPiso;
