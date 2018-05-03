775d36f8a8b2eaf2bd3d38e3d609f9c179b43b27
cceren <cceren@harding.edu>
2018-05-03 14:23:02 -0600

    clase de pago incompleta(calcular monto,verificar,registrar)

---
+++ src/Sistema/Nombre.java
@@ -0,0 +1,37 @@
+
+package Sistema;
+
+/**
+ *
+ * @author Kriz
+ */
+public class Nombre {
+    public Piso piso;
+    private int numero;
+    
+    public Nombre(){}
+
+    public Nombre(Piso piso, int numero) {
+        this.piso = piso;
+        this.numero = numero;
+    }
+
+    
+    public Piso getPiso() {
+        return piso;
+    }
+
+    public void setPiso(Piso piso) {
+        this.piso = piso;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+   
+    
+}
