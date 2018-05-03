f06837deabdc260f8b9131f395fdaa0636b29a7e
Julio Flores <00081817@uca.edu.sv>
2018-05-03 15:02:38 -0600

    Se modifico la clase registro reserva

--- src/Sistema/RegistroReserva.java
+++ src/Sistema/RegistroReserva.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  *
  * @author Julio Flores <00081817@uca.edu.sv>
  */
-public class RegistroReserva {
+public class RegistroReserva extends Reserva{
     private final ArrayList<Reserva> reservas;
     
     public RegistroReserva(){
