2c1691da7a3720bc36bae0e574f99d867b89a21b
Juliof <>
2018-05-02 23:54:07 -0600

    Se agrego la clase Registro de Reserva

---
+++ src/RegistroReserva.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+        
+/**
+ *
+ * @author Julio Flores <00081817@uca.edu.sv>
+ */
+public class RegistroReserva {
+    private ArrayList<Reserva> reservas;
+    
+    public RegistroReserva(){
+        reservas = new ArrayList<>();
+    }
+}
