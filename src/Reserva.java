77777aa49e6f3c8c6d930d50e7a9946fe2f8b50f
PC <PC@W8>
2018-05-02 23:32:16 -0600

    Se agrego la clase Reserva

---
+++ src/Reserva.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Julio Flores <00081817@uca.edu.sv>
+ */
+public class Reserva {
+    //Atrubutos
+    public String codigo;
+    public String fechaent;
+    public String fechasal;
+    public boolean estado;
+    public int dias;
+    public String paquete;
+    //Constructor
+    public Reserva(){}
+    //Metodos
+    public Reserva( String codigo, String fechaent, String fechasal, boolean estado, int dias, String paquete){
+        this.codigo = codigo;
+        this.fechaent = fechaent;
+        this.fechasal = fechasal;
+        this.estado = estado;
+        this.dias = dias;
+        this.paquete = paquete;
+    }
+    //Setters and getters
+    public String getCodigo(){
+        return codigo;
+    } 
+    
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+    
+    public String getFechaent(){
+        return fechaent;
+    }
+
+    public void setFechaent(String fechaent){
+        this.fechaent = fechaent;
+    }
+    
+    public String getFechasal(){
+        return fechasal;
+    }
+    
+    public void setFechasal(String fechasal){
+        this.fechasal = fechasal;
+    }
+    
+    public boolean getEstado(){
+        return estado;
+    }
+    
+    public void setEstado(boolean estado){
+        this.estado = estado;
+    }
+    
+    public int getDias(){
+        return dias;
+    }
+    
+    public void setDias(int dias){
+        this.dias = dias;
+    }
+    
+    public String getPaquete(){
+        return paquete;
+    }
+    
+    public void setPaquete(String paquete){
+        this.paquete = paquete;
+    }
+}
