3ea1e86fe7f8ee65c8a02752645784d91681185d
cceren <cceren@harding.edu>
2018-05-03 00:30:33 -0600

    clase de pago incompleta(calcular monto,verificar,registrar)

--- src/Sistema/Pago.java
+++ src/Sistema/Pago.java
@@ -15,5 +15,55 @@ public class Pago {
     public Piso CostoPiso;
     public Paquete CostoPaquete;
 */
+    public Pago(){}
+    
+    public Pago(double MontoTotal, Fecha FechaPago, Cliente Cliente, Habitacion CostoHabitacion, String Codigo) {
+        this.MontoTotal = MontoTotal;
+        this.FechaPago = FechaPago;
+        this.Cliente = Cliente;
+        this.CostoHabitacion = CostoHabitacion;
+        this.Codigo = Codigo;
+    }
+    
+    public double getMontoTotal() {
+        return MontoTotal;
+    }
+
+    public void setMontoTotal(double MontoTotal) {
+        this.MontoTotal = MontoTotal;
+    }
+
+    public Fecha getFechaPago() {
+        return FechaPago;
+    }
+
+    public void setFechaPago(Fecha FechaPago) {
+        this.FechaPago = FechaPago;
+    }
+
+    public Cliente getCliente() {
+        return Cliente;
+    }
+
+    public void setCliente(Cliente Cliente) {
+        this.Cliente = Cliente;
+    }
+
+    public Habitacion getCostoHabitacion() {
+        return CostoHabitacion;
+    }
+
+    public void setCostoHabitacion(Habitacion CostoHabitacion) {
+        this.CostoHabitacion = CostoHabitacion;
+    }
+
+    public String getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(String Codigo) {
+        this.Codigo = Codigo;
+    }
+    
     
 }
