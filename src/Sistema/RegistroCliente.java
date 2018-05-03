811884a4b67c9044f27b44e10f88cf095a03e1f1
cceren <cceren@harding.edu>
2018-05-03 17:43:12 -0600

    gettes setters

--- src/Sistema/RegistroCliente.java
+++ src/Sistema/RegistroCliente.java
@@ -5,6 +5,7 @@
  */
 package Sistema;
 import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author Kriz
@@ -18,7 +19,46 @@ public class RegistroCliente extends Cliente{
     public RegistroCliente(){
         Clientes = new ArrayList<>();
     }
-    
+    public void add(){
+        
+        Cliente cliente = new Cliente();
+        //Nombre nombre= new Nombre();
+        Scanner input=new Scanner(System.in);
+        
+        System.out.println("ID:");
+        String cadena=input.nextLine();
+        cliente.setNombre(cadena);
+        
+        System.out.println("Nombre:");
+        String nombre=input.nextLine();
+        cliente.setNombre(nombre);
+        
+        System.out.println("Apellido:");
+        String apellido=input.nextLine();
+        cliente.setNombre(apellido);
+        int i=0;
+        while(i>2){
+        System.out.println("cantidad de habitaciones?: (maximo 2)");
+        i=input.nextInt();
+        break;
+        
+        
+        System.out.println("Nombre:");
+        String cadena=input.nextLine();
+        cliente.setNombre(cadena);
+        System.out.println("Nombre:");
+        String cadena=input.nextLine();
+        cliente.setNombre(cadena);
+        System.out.println("Nombre:");
+        String cadena=input.nextLine();
+        cliente.setNombre(cadena);
+        System.out.println("Nombre:");
+        String cadena=input.nextLine();
+        cliente.setNombre(cadena);
+        
+        
+        Clientes.add(cliente);
+    }
     
     
 }
