3350fb5fd0ceaf793a30065e4f31cfe3e78e1f72
Diego Vega <00148816@uca.edu.sv>
2018-05-04 01:37:26 -0600

    Eliminando Herencia innecesaria

--- src/Sistema/RegistroReserva.java
+++ src/Sistema/RegistroReserva.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  *
  * @author Julio Flores <00081817@uca.edu.sv>
  */
-public class RegistroReserva extends Reserva{
+public class RegistroReserva{
     private final ArrayList<Reserva> reservas;
     
     public RegistroReserva(){
