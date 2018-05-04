58943ec8ae25e9c2537e6b2b201c0dcac873f47c
Julio Flores <00081817@uca.edu.sv>
2018-05-04 11:10:55 -0600

    Corrigiendo los metodos

--- src/Sistema/Paquete.java
+++ src/Sistema/Paquete.java
@@ -1,8 +1,4 @@
-m/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Sistema;
 
 import java.util.ArrayList;
