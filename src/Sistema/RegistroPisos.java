de1d21af5c2de6e21feeb3929460b92f0438455a
Julio Flores <00081817@uca.edu.sv>
2018-05-03 23:47:34 -0600

    Agregando mas metodos

--- src/Sistema/RegistroPisos.java
+++ src/Sistema/RegistroPisos.java
@@ -1,7 +1,7 @@
 package Sistema;
 
 import java.util.ArrayList;
-
+import java.util.Scanner;
 /**
  *
  * @author Julio Flores <00081817@uca.edu.sv>
@@ -14,9 +14,23 @@ public class RegistroPisos extends Piso{
         pisos = new ArrayList<>();
     }
     
-    public void add(Piso NombrePiso){
+    public void add(){
         Piso piso = new Piso();
         
-        pisos.add(Piso)
+        pisos.add(piso);
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese el costo del piso: ");
+        piso.setCosto(leer.nextDouble());
+        
+        System.out.println("Ingrese el estado en que se encuentra el piso de donde se quiere hacer la reserva: ");
+        piso.setEstado(true);
+        
+        System.out.println("Ingrese el piso en que se enceuntra la habitacion: ");
+        piso.setPiso();
+    }
+    
+    public void modificar(){
+        
     }
 }
