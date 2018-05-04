d1b40aebe7849a0ac9c87a23e4157c22ea8c3113
Diego Vega <00148816@uca.edu.sv>
2018-05-04 01:38:35 -0600

    Eliminando Herencia innecesaria, confusion

--- src/Sistema/RegistroPisos.java
+++ src/Sistema/RegistroPisos.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * @author Julio Flores <00081817@uca.edu.sv>
  */
 //Esta esperando que se cree la clase piso...
-public class RegistroPisos extends Piso{
+public class RegistroPisos{
     private final ArrayList<Piso> pisos;
     
     public RegistroPisos(){
