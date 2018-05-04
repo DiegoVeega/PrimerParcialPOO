3ce2884e8c0184f1844120803522bf24ccf44b03
Julio Flores <00081817@uca.edu.sv>
2018-05-04 11:22:20 -0600

    Haciendo la clase Paqeuet Basico

--- src/Sistema/PaqueteBasico.java
+++ src/Sistema/PaqueteBasico.java
@@ -1,16 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Sistema;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author Rafael Melara
  */
 public class PaqueteBasico {
+    private double costo;
+    ArrayList<String> servicios = new ArrayList<String>();
     
+    public PaqueteBasico(){}
     
     
 }
