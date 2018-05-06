a6d1443345a27c746999a34eba7e2adfd5a71d22
Rafael Melara <Rafael Melara@DESKTOP-ICRJ4VN>
2018-05-05 20:53:59 -0600

    Solucionando errores de salida

--- src/Sistema/RegistroHabitacion.java
+++ src/Sistema/RegistroHabitacion.java
@@ -36,7 +36,7 @@ public class RegistroHabitacion{
     public void remove(){
         Habitacion habitacion=new Habitacion();
         int nume;
-        System.out.println("Segun la lista mostrada luego de ingresar un producto, ingrese el numero correspondiente segun fila del producto que desea remover./n NOTA: LA LISTA EMPIEZA CON EL NUMERO 0. ");
+        System.out.println("Segun la lista mostrada luego de ingresar un producto, ingrese el numero correspondiente segun fila de la habitacion que desea remover.\n NOTA: LA LISTA EMPIEZA CON EL NUMERO 0. ");
         Scanner num=new Scanner(System.in);
         nume=num.nextInt();
         habitaciones.remove(nume);
@@ -47,14 +47,16 @@ public class RegistroHabitacion{
         int num;
         String nom;
         Scanner cambio=new Scanner(System.in);
-        System.out.println("Segun la lista mostrada luego de ingresar un registro, ingrese el numero correspondiente segun fila del producto que desea modificar./n NOTA: LA LISTA EMPIEZA CON EL NUMERO 0. ");
+        System.out.println("Segun la lista mostrada luego de ingresar un registro, ingrese el numero correspondiente segun fila de la habitacion  que desea modificar.\n NOTA: LA LISTA EMPIEZA CON EL NUMERO 0. ");
         num=cambio.nextInt();
+        
+        habitaciones.get(num).setEstado(Boolean.FALSE);
         
         System.out.println("Ingrese Tipo de habitacion: ");
         habitaciones.get(num).setTipoHabitacion(cambio.nextLine());
         
         
-        habitaciones.get(num).setEstado(Boolean.FALSE);
+        
         
         System.out.println("Ingrese nuevo precio para la habitacion: ");
         habitaciones.get(num).setPrecio(cambio.nextDouble());
