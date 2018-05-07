4b270e058d4b9e588ead2500118456fc100477f8
Diego Vega <00148816@uca.edu.sv>
2018-05-07 14:23:25 -0600

    Trbajando en metodo add para agregar contenido a la lista sin ser desde teclado

--- src/Sistema/RegistroHabitacion.java
+++ src/Sistema/RegistroHabitacion.java
@@ -8,70 +8,105 @@ import java.util.Set;
  *
  * @author Diego Vega <00148816@uca.edu.sv>
  */
-public class RegistroHabitacion{
+public class RegistroHabitacion {
+
     //Atributos.
     private final ArrayList<Habitacion> habitaciones;
-    
-    public RegistroHabitacion(){
-        habitaciones=new ArrayList<>();
+
+    public RegistroHabitacion() {
+        habitaciones = new ArrayList<>();
     }
+
     //Metodos.
-    public void add(){
+    public void add() {
         Habitacion habitacion = new Habitacion();
-        
+
         habitaciones.add(habitacion);
-        Scanner leer=new Scanner(System.in);
-        //PARA SABER EL TIPO DE HABITACION SE PIENSA AGREGAR UN METODO QUE VERIFIQUE SI EL NUMERO DE HABITACION ES PAR O NO.
-        System.out.println("Ingrese tipo de habitacion, Doble o Sencilla.");
-        habitacion.setTipoHabitacion(leer.nextLine());
-        
-       
-        habitacion.setEstado(Boolean.TRUE);
-        
-        System.out.println("Ingrese el precio de la habitacion: ");
-        habitacion.setPrecio(leer.nextDouble());
-        //SET ESTADO
+/**
+        habitaciones.get(0).setTipoHabitacion("Sencilla");
+        habitaciones.get(0).setEstado(Boolean.TRUE);
+        //VARIABLES GLOBALES.
+        habitaciones.get(0).setPrecio(200.00);
+        */
+
+        //MEJORANDO PROGRAMA.
+        for (int i = 0; i <= 59; i++) {
+            if (i == 0) {
+                habitacion.setTipoHabitacion("Sencilla");
+                habitacion.setEstado(Boolean.TRUE);
+                //VARIABLES GLOBALES.
+                habitacion.setPrecio(200.00);
+                i++;
+            }
+            //INICIA EN 0, LOS PARES SON LAS HABITACIONES IMPARES
+            if (i % 2 == 0) {
+                habitacion.setTipoHabitacion("Sencilla");
+                habitacion.setEstado(Boolean.TRUE);
+                //VARIABLES GLOBALES.
+                habitacion.setPrecio(200.00);
+                i++;
+            } else {
+                habitacion.setTipoHabitacion("Doble");
+                habitacion.setEstado(Boolean.TRUE);
+                //VARIABLES GLOBALES.
+                habitacion.setPrecio(300.00);
+                i++;
+            }
     }
-    
-    public void remove(){
-        Habitacion habitacion=new Habitacion();
+    }
+    /**
+     * Scanner leer=new Scanner(System.in); //PARA SABER EL TIPO DE HABITACION
+     * SE PIENSA AGREGAR UN METODO QUE VERIFIQUE SI EL NUMERO DE HABITACION ES
+     * PAR O NO. System.out.println("Ingrese tipo de habitacion, Doble o
+     * Sencilla."); habitacion.setTipoHabitacion(leer.nextLine());
+     *
+     *
+     * habitacion.setEstado(Boolean.TRUE);
+     *
+     * System.out.println("Ingrese el precio de la habitacion: ");
+     * habitacion.setPrecio(leer.nextDouble()); //SET ESTADO
+     */
+
+
+public void remove() {
+        Habitacion habitacion = new Habitacion();
         int nume;
         System.out.println("Segun la lista mostrada luego de ingresar un producto, ingrese el numero correspondiente segun fila de la habitacion que desea remover.\n NOTA: LA LISTA EMPIEZA CON EL NUMERO 0. ");
-        Scanner num=new Scanner(System.in);
-        nume=num.nextInt();
+        Scanner num = new Scanner(System.in);
+        nume = num.nextInt();
         habitaciones.remove(nume);
     }
-    
-    public void set(){
-        Habitacion habitacion=new Habitacion();
+
+    public void set() {
+        Habitacion habitacion = new Habitacion();
         int num;
         String nom;
-        Scanner cambio=new Scanner(System.in);
+        Scanner cambio = new Scanner(System.in);
         System.out.println("**********SE MODIFICO ESTADO DEL PISO AUTOMATICAMENTE*************");
         System.out.println("Segun la lista mostrada luego de ingresar un registro, ingrese el numero correspondiente segun fila de la habitacion  que desea modificar.\n NOTA: LA LISTA EMPIEZA CON EL NUMERO 0. ");
-        num=cambio.nextInt();
-        
+        num = cambio.nextInt();
+
         habitaciones.get(num).setEstado(Boolean.FALSE);
-        
+
         System.out.println("Ingrese Tipo de habitacion: ");
         habitaciones.get(num).setTipoHabitacion(cambio.nextLine());
-        
-        
-        
-        
+
         System.out.println("Ingrese nuevo precio para la habitacion: ");
         habitaciones.get(num).setPrecio(cambio.nextDouble());
         //A TRAVES DE CONDICION PARA SIMPLIFICAR.
     }
-    public void mostrar(){
+
+    public void mostrar() {
         //Orden
-        for(Habitacion e:habitaciones){
-            System.out.println("Tipo de habitacion: "+e.getTipoHabitacion());
-            System.out.print(" ");
-            System.out.print("Estado: "+e.getEstado());
-            System.out.println(" ");
-            System.out.print("Precio: $"+e.getPrecio());
+        for (Habitacion e : habitaciones) {
+            System.out.println("Tipo de habitacion: " + e.getTipoHabitacion() + " Estado: " + e.getEstado() + " Precio: $" + e.getPrecio());
             System.out.println("\n");
+            /**
+             * System.out.print(" "); System.out.print("Estado:
+             * "+e.getEstado()); System.out.println(" ");
+             * System.out.print("Precio: $"+e.getPrecio());
+             * System.out.println("\n");
+             */
         }
     }
 }
